57dddbd7287c1da34b4a93157f5a1cea1302ea0f
Kashyap Patel <patel123kashyap@gmail.com>
2023-02-26 23:38:46 +0530

    Distinct Numbers Added

---
+++ Sorting and Searching/Distinct Numbers.cpp
@@ -0,0 +1,26 @@
+/** Kashyap Patel **/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// here we use set for the distinct number
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n; cin >> n;
+    set<int> s;
+
+    for (int i = 0, x; i < n; i++) {
+        cin >> x;
+        s.insert(x);
+    }
+
+    cout << (int)s.size() << endl;
+
+    // TC : O(nlogn)
+    // SC : O(n)
+}
