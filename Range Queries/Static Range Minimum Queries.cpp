6e9174e74f4347f59bf6f1b9ff0e56d31894ac63
Kashyap Patel <patel123kashyap@gmail.com>
2023-06-21 00:05:20 +0530

    Static Range Minimum Queries added

---
+++ Range Queries/Static Range Minimum Queries.cpp
@@ -0,0 +1,30 @@
+/** Kashyap Patel **/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, Q;
+    cin >> n >> Q;
+
+    vector<long long> prefix(n + 1);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> prefix[i];
+        prefix[i] += prefix[i - 1];
+    }
+
+    for (int q = 0; q < Q; q++) {
+        int a, b;
+        cin >> a >> b;
+        cout << prefix[b] - prefix[a - 1] << '\n';
+    }
+    // TC : O(n * x)
+    // SC : O(x)
+}
